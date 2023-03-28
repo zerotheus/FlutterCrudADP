3347bf745aaf41d4d6a1bd601d9133be1e2bbe7a
Ntheus <104659816+zerotheus@users.noreply.github.com>
2023-03-28 16:45:56 -0300

    provider usuarios

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,3 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:core';
 
-class Users with ChangeNotifier {}
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_adp/data/placeholders_alunos.dart';
+import 'package:flutter_crud_adp/data/placeholders_disciplinas.dart';
+import 'package:flutter_crud_adp/data/placeholders_professores.dart';
+import 'package:flutter_crud_adp/models/aluno.dart';
+import 'package:flutter_crud_adp/models/disciplinas.dart';
+import 'package:flutter_crud_adp/models/professor.dart';
+
+class Users with ChangeNotifier {
+  final Map<String, Aluno> _alunos = {...placeholdersAlunos};
+  final Map<String, Professor> _professores = {...placeholderProfessores};
+  final Map<String, Disciplina> _disciplinas = {...placeholderDisciplinas};
+
+  List<Aluno> get TodosAlunos {
+    return [..._alunos.values];
+  }
+
+  int get countAlunos {
+    return _alunos.length;
+  }
+}
