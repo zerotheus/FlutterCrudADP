6342efd89cbe161d97533251110e8a697b91fdff
Ntheus <104659816+zerotheus@users.noreply.github.com>
2023-03-30 16:51:19 -0300

    modificao no place older da disciplina

--- lib/data/placeholders_disciplinas.dart
+++ lib/data/placeholders_disciplinas.dart
@@ -5,13 +5,10 @@ import '../models/disciplinas.dart';
 
 const professores = {...placeholderProfessores};
 
-const placeholderDisciplinas = {
-  '4': const Disciplina(
-      nome: "INF",
-      codigo: "000",
-      professor: const Professor(
-          cpf: "654",
-          nome: "aria",
-          avatarUrl:
-              "https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404__340.png")),
+Map<String, Disciplina> placeholderDisciplinas = {
+  '4': Disciplina(
+    nome: "INF",
+    codigo: "000",
+    professor: null,
+  ),
 };
